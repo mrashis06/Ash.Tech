fa1707a20bcbd5c5730b64255a67bb6a51e2642e
Mr Ashis <mrashis0603@gmail.com>
2025-08-30 21:34:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="grid g

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
 import { SkillProgressBar } from './skill-progress-bar';
+import { GCPLogo, CLogo, JavaLogo, JSLogo, MongoDBLogo, NextJSLogo, NodeJSLogo, PythonLogo, ReactLogo, SQLLogo, TSLogo } from './skill-icons';
 
 const skills = [
-  { name: 'Python', level: 90 },
-  { name: 'JavaScript / TypeScript', level: 85 },
-  { name: 'React / Next.js', level: 88 },
-  { name: 'Node.js', level: 80 },
-  { name: 'Java', level: 85 },
-  { name: 'C', level: 75 },
-  { name: 'MongoDB', level: 82 },
-  { name: 'SQL', level: 82 },
+  { name: 'Python', level: 90, icon: <PythonLogo className="w-8 h-8 text-primary transition-colors group-hover:text-[#3776AB]" /> },
+  { name: 'JavaScript / TypeScript', level: 85, icon: <div className="flex gap-2"><JSLogo className="w-8 h-8 text-primary transition-colors group-hover:text-[#F7DF1E]" /><TSLogo className="w-8 h-8 text-primary transition-colors group-hover:text-[#3178C6]" /></div> },
+  { name: 'React / Next.js', level: 88, icon: <div className="flex gap-2"><ReactLogo className="w-8 h-8 text-primary transition-colors group-hover:text-[#61DAFB]" /><NextJSLogo className="w-8 h-8 text-primary transition-colors group-hover:text-white" /></div> },
+  { name: 'Node.js', level: 80, icon: <NodeJSLogo className="w-8 h-8 text-primary transition-colors group-hover:text-[#339933]" /> },
+  { name: 'Java', level: 85, icon: <JavaLogo className="w-8 h-8 text-primary transition-colors group-hover:text-[#007396]" /> },
+  { name: 'C', level: 75, icon: <CLogo className="w-8 h-8 text-primary transition-colors group-hover:text-[#A8B9CC]" /> },
+  { name: 'MongoDB', level: 82, icon: <MongoDBLogo className="w-8 h-8 text-primary transition-colors group-hover:text-[#47A248]" /> },
+  { name: 'SQL', level: 82, icon: <SQLLogo className="w-8 h-8 text-primary transition-colors group-hover:text-[#4479A1]" /> },
+  { name: 'Google Cloud', level: 78, icon: <GCPLogo className="w-8 h-8 text-primary transition-colors group-hover:text-[#4285F4]" /> },
 ];
 
 export function AboutSection() {
@@ -36,9 +38,14 @@ export function AboutSection() {
             </p>
             <div className="space-y-4">
               <h3 className="text-2xl font-bold">My Skills</h3>
-              <div className="grid gap-4">
+              <div className="grid gap-6">
                 {skills.map((skill) => (
-                  <SkillProgressBar key={skill.name} skill={skill.name} level={skill.level} />
+                  <div key={skill.name} className="flex items-center gap-4 group">
+                    <div className="w-12 h-12 flex items-center justify-center">{skill.icon}</div>
+                    <div className="w-full">
+                      <SkillProgressBar skill={skill.name} level={skill.level} />
+                    </div>
+                  </div>
                 ))}
               </div>
             </div>
---
+++ src/components/skill-icons.tsx
@@ -0,0 +1,79 @@
+// A collection of SVG logos for skills.
+
+export const PythonLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.24 13.76c.4.4.4 1.05 0 1.45s-1.05.4-1.45 0l-2.79-2.79-2.79 2.79c-.4.4-1.05.4-1.45 0s-.4-1.05 0-1.45L10.55 12l-2.79-2.79c-.4-.4-.4-1.05 0-1.45s1.05-.4 1.45 0l2.79 2.79 2.79-2.79c.4-.4 1.05-.4 1.45 0s.4 1.05 0 1.45L13.45 12l2.79 2.79z" fill="currentColor"/>
+  </svg>
+);
+
+export const JSLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M0 0h24v24H0z" fill="currentColor"/>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1.5 15h-3v-2.5h3V17zm0-4h-3v-2.5h3V13zm0-4h-3V6.5h3V9zm5.5 8h-3V15h3v2zm0-4h-3v-2.5h3V13z" fill="#000"/>
+  </svg>
+);
+
+export const TSLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M2 12C2 6.48 6.48 2 12 2s10 4.48 10 10-4.48 10-10 10S2 17.52 2 12zm10-5.5H8v1.5h4v1.5h-2.5V17h-1.5v-5.5H8V10h4V6.5zm4.5 9h-3V14h3v1.5z" fill="currentColor"/>
+  </svg>
+);
+
+export const ReactLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="-11.5 -10.23174 23 20.46348" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <circle cx="0" cy="0" r="2.05" fill="currentColor"/>
+    <g stroke="currentColor" strokeWidth="1" fill="none">
+      <ellipse rx="11" ry="4.2"/>
+      <ellipse rx="11" ry="4.2" transform="rotate(60)"/>
+      <ellipse rx="11" ry="4.2" transform="rotate(120)"/>
+    </g>
+  </svg>
+);
+
+export const NextJSLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <circle cx="64" cy="64" r="64" fill="#000"/>
+    <path d="M96.79 34.24c-3.13-2.5-12.72-2.5-15.85 0-3.13 2.5-3.13 6.57 0 9.07l16.3 13.15c.53.42 1.08.82 1.63 1.2v32.58c0 2.27 2.6 3.4 4.3 2.03V48.1a6.6 6.6 0 00-2.2-4.72L84.67 34.24a6.6 6.6 0 00-4.45-2.03c-1.63 0-3.27.7-4.43 2.03l-31.5 25.1c-2.45 1.95-2.45 5.1 0 7.05l16.3 13.15c2.45 1.95 6.4.6 6.4-2.5V66.23a6.6 6.6 0 00-2.2-4.72L48.5 44.36c-2.45-1.95-6.4-.6-6.4 2.5v35.82c0 2.27 2.6 3.4 4.3 2.03l31.5-25.1c2.45-1.95 2.45-5.1 0-7.05l-16.3-13.15a6.6 6.6 0 00-4.45-2.03 6.6 6.6 0 00-4.45 2.03" fill="currentColor"/>
+  </svg>
+);
+
+
+export const NodeJSLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M12.33 2.04c-5.52 0-10 4.48-10 10s4.48 10 10 10 10-4.48 10-10-4.48-10-10-10zm-1.8 14.53l-3.2-3.68v3.52h-1.55V7.16h1.55v3.68l3.1-3.68h1.8L10.2 11l3.53 5.57h-1.8zM18 10.16h-1.5v-1.5h-1.5v1.5H13.5v1.5h1.5v1.5h1.5v-1.5H18v-1.5z" fill="currentColor"/>
+  </svg>
+);
+
+
+export const JavaLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4 13h-2.8l-2-3.15v3.15H9V9h2.8l2 3.15V9H16v6z" fill="currentColor"/>
+  </svg>
+);
+
+export const CLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-2-12h4v1.5h-4V8zm0 3h4v1.5h-4v-1.5zm0 3h4V16h-4v-1.5z" fill="currentColor"/>
+  </svg>
+);
+
+
+export const MongoDBLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 14.5c-2.49 0-4.5-2.01-4.5-4.5S9.51 7.5 12 7.5s4.5 2.01 4.5 4.5-2.01 4.5-4.5 4.5z" fill="currentColor"/>
+    <path d="M12 9.5c-1.38 0-2.5 1.12-2.5 2.5s1.12 2.5 2.5 2.5 2.5-1.12 2.5-2.5-1.12-2.5-2.5-2.5z" fill="currentColor"/>
+  </svg>
+);
+
+export const SQLLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 14H9v-2h2v2zm0-4H9v-2h2v2zm0-4H9V6h2v2zm4 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V6h2v2z" fill="currentColor"/>
+  </svg>
+);
+
+export const GCPLogo = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1.12 14.12L7 12.25l3.88-3.87 3.87 3.87-3.87 3.87z" fill="currentColor"/>
+    <path d="M10.88 16.12l-3.88-3.87 3.88-3.87 1.12 1.12-2.75 2.75 2.75 2.75-1.12 1.12zm2.24-1.12l2.75-2.75-2.75-2.75 1.12-1.12 3.88 3.87-3.88 3.87-1.12-1.12z" fill="currentColor"/>
+  </svg>
+);
