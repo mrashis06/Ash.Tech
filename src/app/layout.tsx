9f22574e4c66cd3a16b8c8b96ca6648b2738e391
Mr Ashis <mrashis0603@gmail.com>
2025-09-01 08:17:27 +0000

    The previous app was installed and it is in app section but now after in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><rect width=%22100%22 height=%22100%22 rx=%2215%22 fill=%22hsl(180 100% 40%)%22></rect><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2260%22 font-family=%22monospace%22 fill=%22hsl(240 10% 3.9%)%22>AT</text></svg>" />
+        <link rel="icon" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Source+Code+Pro:wght@400;600;700&display=swap" rel="stylesheet" />
